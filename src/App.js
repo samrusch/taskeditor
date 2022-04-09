eacfd51a9ffea8a5c85192670b96cb91175408fb
Sam Rusch <samrusch@gmail.com>
2022-04-09 11:52:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// Components
+import TaskReader from './components/TaskReader'
+//import TaskEditor from './components/TaskEditor'
+import TaskCard from './components/TaskCard'
 
 function App() {
+  const sampleData = {
+    "name": "Task name",
+    "description": "Description of this task"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='my-10'>
+      <TaskCard data={sampleData} />
     </div>
   );
 }
